4c6dcf82acd941b9676fcf419d12adf59398e670
Grandis Rufaidah <grandisrufaidah99@gmail.com>
2017-01-13 12:19:43 -0800

    Add project files.

---
+++ GRANDIS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GRANDIS.Startup))]
+namespace GRANDIS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
